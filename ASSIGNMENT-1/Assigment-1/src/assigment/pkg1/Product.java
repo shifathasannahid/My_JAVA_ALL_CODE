518fd34347736795feb1a3594639bd4f494b9abc
shifat <shifathasannahid@gmail.com>
2024-02-08 12:25:32 +0600

    Complete Assignment-1

---
+++ ASSIGNMENT-1/Assigment-1/src/assigment/pkg1/Product.java
@@ -0,0 +1,15 @@
+
+package assigment.pkg1;
+
+public class Product {
+    
+    public static void main(String[] args) 
+    {
+        System.out.println("id: 101,");
+        System.out.println("title: ihone15,");
+        System.out.println("price: 1895 euros,");
+        System.out.println("description: perfect pproduct with best image quality,");
+        System.out.println("category: phone,");
+    }
+    
+}
