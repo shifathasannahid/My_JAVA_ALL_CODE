6fea5afaed9a6aa333562636b439e4362ea8596e
shifat <shifathasannahid@gmail.com>
2024-02-11 07:24:33 +0600

    Assigment-1

---
+++ BasicJava/src/basicjava/BioData.java
@@ -0,0 +1,16 @@
+
+package basicjava;
+public class BioData {
+     
+    public static void main(String[] args) 
+    {
+       System.out.println(" \"Shifat \t Hasan\" ");
+       System.out.println(" \"Nahid \t Jamil\"");
+       
+     
+    }
+    
+}
+
+
+
