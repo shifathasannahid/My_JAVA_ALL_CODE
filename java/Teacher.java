feaefd980164140044fb88b194a84e25564627f9
shifat <shifathasannahid@gmail.com>
2024-02-14 22:03:17 +0600

    Java New File

---
+++ java/Teacher.java
@@ -0,0 +1,21 @@
+
+public class Teacher{
+    String name,Gender;
+    int phone;
+
+    public static void main(String[] args)
+    {
+     Teacher teacher1;//object declare
+     teacher1 = new Teacher();//object create
+     teacher1.name ="Shifat Hasan";
+     teacher1.Gender ="Male";
+     teacher1.phone =1938627609;
+     
+     
+     System.out.println("Name :"+teacher1.name);
+      System.out.println("Gender :"+teacher1.Gender);
+       System.out.println("phone :"+teacher1. phone);
+     
+    }
+    
+}
