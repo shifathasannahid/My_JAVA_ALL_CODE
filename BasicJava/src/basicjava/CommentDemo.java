a04e052df3cea17bc3793df40a5df920888ff638
shifat <shifathasannahid@gmail.com>
2024-02-14 10:29:23 +0600

    6.8 video comment topic

---
+++ BasicJava/src/basicjava/CommentDemo.java
@@ -0,0 +1,22 @@
+/*
+Shifat hasan
+Bio Data
+
+ */
+package basicjava;
+
+
+ 
+public class CommentDemo {
+    public static void main(String[] args)
+    {
+      
+     System.out.println("Shifat"); //Dislaying my name 
+     
+     //Displaying my year
+     System.out.println("23 years old");
+    }
+    
+}
+
+
