87706accdb538d7058ee6ace5c68c7900bfd4eba
shifat <shifathasannahid@gmail.com>
2024-02-15 10:20:37 +0600

    Method theke kibabe Value Return korte hoi

---
+++ java/Returnting_Value.java
@@ -0,0 +1,19 @@
+public class Returnting_Value
+{
+    int square(int value)
+    {
+        return value*value;
+    }
+
+    public static void main(String[] args) {
+
+        Returnting_Value ob1 = new Returnting_Value();
+        System.out.println("Squre of 5 = "+ob1.square(5));
+       
+
+        Returnting_Value ob2 = new Returnting_Value();
+        System.out.println("Squre of 8 = "+ob1.square(8));
+        
+    }
+
+}
