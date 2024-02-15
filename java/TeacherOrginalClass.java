470c1d25d09f4031b2059767979808e9d31a78a0
shifat <shifathasannahid@gmail.com>
2024-02-15 08:28:32 +0600

    Default Constractor er Use

--- java/TeacherOrginalClass.java
+++ java/TeacherOrginalClass.java
@@ -3,6 +3,11 @@ public class TeacherOrginalClass{
     String name,Gender;
     int phone;
 
+    TeacherOrginalClass()//Default Constractor
+    {
+        System.out.println("No Value");
+    }
+
     TeacherOrginalClass(String n,String g,int ph)//Ata ke perameterized Constractor bole
     {
         name = n;
@@ -23,14 +28,17 @@ public class TeacherOrginalClass{
     public static void main(String[] args)
     {
      
-    TeacherOrginalClass teacher1 = new TeacherOrginalClass("Shifat Hasan","Male",1938627609);//object create
+    TeacherOrginalClass teacher1 = new TeacherOrginalClass("Shifat Hasan","Male",1938627609);//object create and constractor call hoice
      teacher1.DisplayInforfation();
 
 
 
-    TeacherOrginalClass teacher2= new TeacherOrginalClass("Hasan Jamil","Male",193865709);//object create
+    TeacherOrginalClass teacher2= new TeacherOrginalClass("Hasan Jamil","Male",193865709);//object create and constractor call hoice
        
-       teacher2.DisplayInforfation();
+    teacher2.DisplayInforfation();
+
+    TeacherOrginalClass teacher3 =new TeacherOrginalClass();
+    teacher3.DisplayInforfation();
        
        
        
