e0d8277f420d28524439c98ca1f510ce0c09cb3a
shifat <shifathasannahid@gmail.com>
2024-02-15 14:39:45 +0600

    Static use kore count update

--- java/Student_Static_Keyword.java
+++ java/Student_Static_Keyword.java
@@ -1,14 +1,30 @@
 public class Student_Static_Keyword
 {
     
-    static String universityName ="BUBT";
+   static int count=0;
 
+Student_Static_Keyword()
+{
+    count++;
+}
 
+void TotalStudent()
+{
+    System.out.println("Total Student = "+count);
+}
 
 
 public static void main(String[] args) {
 
-System.out.println("Name :"+Student_Static_Keyword.universityName);   
+Student_Static_Keyword s1 =new Student_Static_Keyword();
+s1.TotalStudent();
+
+Student_Static_Keyword s2 =new Student_Static_Keyword();
+s2.TotalStudent();
+
+Student_Static_Keyword s3 =new Student_Static_Keyword();
+s3.TotalStudent();
+
 
 }
 
