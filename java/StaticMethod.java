9fdd6fc87d9a1eaa995b10a44ab393cf20a467a8
shifat <shifathasannahid@gmail.com>
2024-02-16 06:45:52 +0600

    Static method

--- java/StaticMethod.java
+++ java/StaticMethod.java
@@ -1,12 +1,14 @@
 public class StaticMethod {
 
-    void display1()
+    void display1()//non static method
     {
+        display2();
         System.out.println("I am non static method");
     }
 
-  static void display2()
+  static void display2()//static method
     {
+        
         System.out.println("I am static method");
     }
 
@@ -14,6 +16,6 @@ public class StaticMethod {
         StaticMethod ob1 = new StaticMethod();
         ob1.display1();
 
-        StaticMethod.display2();
+        StaticMethod.display2();//ata static method tai er kunu object create kora lage nai
     }
 }
